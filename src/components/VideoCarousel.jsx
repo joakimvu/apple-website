68cad0a1c5e6db503de41b50322bedb92eb3a783
Joakim Vu <joakimvu@icloud.com>
2024-11-04 11:06:09 +0100

    Update VideoCarousel.jsx

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,5 +1,44 @@
+import { useRef, useState } from "react";
+import { hightlightsSlides } from "../constants";
+
 const VideoCarousel = () => {
-  return <div>VideoCarousel</div>;
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [video, setvideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
+
+  return (
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((list) => (
+          <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video id="video" playsInline={true} preload="auto" muted>
+                  <source src={list.video} type="video/mp4" />
+                </video>
+              </div>
+
+              <div className="absolute top-12 left-[5%] z-10">
+                {list.textLists.map((text) => (
+                  <p key={text} className="md:2xl text-xl font-medium">
+                    {text}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default VideoCarousel;
