aa229d75469eaeee679cc9b772cefcca14c25cea
Joakim Vu <joakimvu@icloud.com>
2024-11-05 18:15:44 +0100

    Create Features.jsx

---
+++ src/components/Features.jsx
@@ -0,0 +1,9 @@
+const Features = () => {
+  return (
+    <section className="h-full common padding bg-zinc overflow-hidden">
+      <div></div>
+    </section>
+  );
+};
+
+export default Features;
