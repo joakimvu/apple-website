4e763f02d095e154e49e6fc88d118ad7eb9e41c6
Joakim Vu <joakimvu@icloud.com>
2024-11-05 22:07:46 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">Iphone 16 Pro</p>
+        <p className="hero-title">Iphone 15 Pro</p>
         <div className="md:w-10/12 w-9/12">
           <video
             autoPlay
